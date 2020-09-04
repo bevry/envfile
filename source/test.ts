b886845fb7a1de0a29b9aff06fb5c72b11cbe860
Benjamin Lupton <b@lupton.cc>
2020-09-04 16:45:32 +1000

    __dirname compat for ESM

--- source/test.ts
+++ source/test.ts
@@ -4,24 +4,26 @@ import kava from 'kava'
 import { exec } from 'safeps'
 import { resolve } from 'path'
 
-const pwd = resolve(__dirname, '..')
+import filedirname from 'filedirname'
+const [file, dir] = filedirname()
+const root = resolve(dir, '..')
 
 // Test
-kava.suite('envfile', function (describe, it) {
-	it('should work without comments', function (done) {
+kava.suite('envfile', function (suite, test) {
+	test('should work without comments', function (done) {
 		const command = `echo "a=1\\nb:2\\nc = 3\\nd : 4" | npx . env2json | npx . json2env`
 		// @ts-ignore
-		exec(command, { cwd: pwd }, function (err, stdout) {
+		exec(command, { cwd: root }, function (err, stdout) {
 			errorEqual(err, null, 'no error to exist')
 			equal(stdout.trim(), 'a=1\nb=2\nc=3\nd=4', 'stdout to be as expected')
 			done()
 		})
 	})
 
-	it('comments should be ignored', function (done) {
+	test('comments should be ignored', function (done) {
 		const command = `echo "#comments with = are ignored\\na=1\\n" | npx . env2json | npx . json2env`
 		// @ts-ignore
-		exec(command, { cwd: pwd }, function (err, stdout) {
+		exec(command, { cwd: root }, function (err, stdout) {
 			errorEqual(err, null, 'no error to exist')
 			equal(stdout.trim(), 'a=1', 'stdout to be as expected')
 			done()
